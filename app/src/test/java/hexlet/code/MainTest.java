350fa4c8566939c59461598a60374e2c288845c3
Sabshor <lsabshor@mail.ru>
2024-06-19 16:02:38 +0300

    add test block

---
+++ app/src/test/java/hexlet/code/MainTest.java
@@ -0,0 +1,12 @@
+package hexlet.code;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class MainTest {
+    @Test
+    public void testEmpty() {
+        assertTrue(true);
+    }
+}
