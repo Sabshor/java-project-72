151bf9824206fb14b702d1ad71a7e7a7aadef015
Sabshor <lsabshor@mail.ru>
2024-06-26 14:34:36 +0300

    add postgress dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,6 +22,7 @@ dependencies {
     //annotationProcessor("org.projectlombok:lombok:1.18.32")
 
     implementation("com.h2database:h2:2.2.220")
+    implementation("org.postgresql:postgresql:42.7.3")
     implementation("com.zaxxer:HikariCP:5.0.1")
     implementation("io.javalin:javalin:6.1.3")
     implementation("org.slf4j:slf4j-simple:2.0.7")
